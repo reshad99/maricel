01da10ccfd6e0107faf146e6a2c2f4591a6cd7b5
Reshad Memmedov <resat619916@gmail.com>
2022-02-23 10:15:58 +0400

    1.2

--- app/Http/Controllers/Front/ReservationController.php
+++ app/Http/Controllers/Front/ReservationController.php
@@ -6,19 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Reservation;
 use App\Models\Register;
+use App\Models\Card;
 use Validator, DB;
 
 class ReservationController extends Controller
 {
     public function index()
-    {        
-        return view('front.reservation');
+    {
+        $cards = Card::all();
+        return view('front.reservation', compact('cards'));
     }
 
     public function post(Request $request)
     {
         try {
-                    if ($request->ajax()) 
+                    if ($request->ajax())
                 {
                     $rules = [
                         'name' => 'required',
@@ -40,7 +42,7 @@ class ReservationController extends Controller
                         $email       = $request->email;
                         $card_number = $request->card_number;
                         $date        = $request->date;
-                        if (Register::where('email', $email)->where('phone', $phone)->where('card_number', $card_number)->exists()) 
+                        if (Register::where('email', $email)->where('phone', $phone)->where('card_number', $card_number)->exists())
                         {
 
                             $reserve = new Reservation;
@@ -50,21 +52,21 @@ class ReservationController extends Controller
                             $reserve->card_number = $card_number;
                             $reserve->date        = $date;
                             $reserve->save();
-    
+
                             return response()->json(['message' => 'Sorğunuz qəbul olundu. Təsdiq mesajı qısa müddət ərzində email-inizə göndəriləcək.'], 200);
                         }
                         else
                         {
                             return response()->json(['error' => 'Bazada belə bir qeydiyyat mövcud deyil.'], 404);
                         }
-                        
-                        
+
+
                     }
                 }
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
-        
+
     }
 
     public function reserved_dates()
